bcebe654a59e192ba482367272effdddcdb82155
matteo-torres <matteotorres27@gmail.com>
2024-08-28 23:13:07 +0000

    combined data set

--- question-research.R
+++ question-research.R
@@ -1,5 +1,6 @@
 # load packages
 library(here)
+library(janitor)
 
 # create a pathway to reteive files from workbench
 data_pathway <- "/courses/EDS214/group_project/2024/tropical-aqua-titans/raw_data"
@@ -8,3 +9,10 @@ data_pathway <- "/courses/EDS214/group_project/2024/tropical-aqua-titans/raw_dat
 prieta_a <- read_csv(here(data_pathway, "QuebradaPrieta-A.csv"))
 prieta_b <- read_csv(here(data_pathway, "QuebradaPrieta-B.csv"))
 prieta <- read_csv(here(data_pathway, "QuebradaPrieta.csv"))
+
+# merge data sets
+combined_prieta <- full_join(prieta_a, prieta_b) %>%
+  full_join(prieta)
+
+# convert to lower snake case
+combined_prieta <- clean_names(combined_prieta)
